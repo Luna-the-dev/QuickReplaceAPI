8d528caf126d97215a75f239c269aebbe6e713b0
Christian Norris <biguitar79@gmail.com>
2024-09-25 22:56:47 -0400

    added copyright header to all files

--- TextReplaceAPI/Core/Helpers/OutputHelper.cs
+++ TextReplaceAPI/Core/Helpers/OutputHelper.cs
@@ -1,4 +1,23 @@
-﻿using DocumentFormat.OpenXml.Packaging;
+﻿/******************************************************************
+ * Copyright (c) QuickReplace, LLC. All Rights Reserved.
+ * 
+ * This file is part of the QuickReplace Developer Library. You may not
+ * use this file for commercial or business use except in compliance with
+ * the QuickReplace license. You may obtain a copy of the license at:
+ * 
+ *   https://www.quickreplace.io/pricing
+ * 
+ * You may view the terms of this license at:
+ * 
+ *   https://www.quickreplace.io/eula
+ * 
+ * This file or any others in the QuickReplace Developer Library may not
+ * be modified, shared, or distributed outside of the terms of the license
+ * agreement by organizations or individuals who are covered by a license.
+ * 
+ *****************************************************************/
+
+using DocumentFormat.OpenXml.Packaging;
 using Wordprocessing = DocumentFormat.OpenXml.Wordprocessing;
 using Spreadsheet = DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml;
--- TextReplaceAPI/QuickReplace.cs
+++ TextReplaceAPI/QuickReplace.cs
@@ -1,4 +1,23 @@
-﻿using TextReplaceAPI.Core.AhoCorasick;
+﻿/******************************************************************
+ * Copyright (c) QuickReplace, LLC. All Rights Reserved.
+ * 
+ * This file is part of the QuickReplace Developer Library. You may not
+ * use this file for commercial or business use except in compliance with
+ * the QuickReplace license. You may obtain a copy of the license at:
+ * 
+ *   https://www.quickreplace.io/pricing
+ * 
+ * You may view the terms of this license at:
+ * 
+ *   https://www.quickreplace.io/eula
+ * 
+ * This file or any others in the QuickReplace Developer Library may not
+ * be modified, shared, or distributed outside of the terms of the license
+ * agreement by organizations or individuals who are covered by a license.
+ * 
+ *****************************************************************/
+
+using TextReplaceAPI.Core.AhoCorasick;
 using TextReplaceAPI.Core.Helpers;
 using TextReplaceAPI.Core.Validation;
 using TextReplaceAPI.DataTypes;
